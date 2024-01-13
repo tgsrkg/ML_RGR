a6f2c288deb491a8b6272cead40cb41e25f45e4c
chapa <83264709+chapaflow@users.noreply.github.com>
2024-01-13 15:11:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def page_predictions():
         input_data = {}
         feature_names =  ['index','time_left','ct_score','t_score','ct_health','t_health','ct_armor','t_armor','ct_money','t_money','ct_helmets','t_helmets','ct_defuse_kits','ct_players_alive','t_players_alive']   
         for feature in feature_names:
-            input_data[feature] = st.number_input(f"{feature} ({feature_descirbe})", min_value=0.0, max_value=100000.0, value=100.0)
+            input_data[feature] = st.number_input(f"{feature} ({feature_descirbe})", min_value=0.0, max_value=100000.0, value=10.0)
 
         if st.button('Сделать предсказание'):
             # Загрузка моделей
